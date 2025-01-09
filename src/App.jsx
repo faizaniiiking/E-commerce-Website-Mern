b1780b6d32bdac096e9e2decfa06d2c9599c0791
faizan kureshi <faizanikkhan@gmail.com>
2025-01-09 16:45:11 +0530

    added some element in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,36 @@
-import React from 'react'
-import { Routes,Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Collection from './pages/Collection'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Product from './pages/Product'
-import Cart from './pages/Cart'
-import Login from './pages/Login'
-import PLaceOrder from './pages/PLaceOrder'
-import Orders from './pages/Orders'
-import Navbar from './components/Navbar'
-
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Collection from "./pages/Collection";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import PLaceOrder from "./pages/PLaceOrder";
+import Orders from "./pages/Orders";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return (
-    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
-      <Navbar/>
+    <div className="px-4  sm:px-[5vw] md:px-[7vw] lg:px-[9vw] ">
+      <Navbar />
 
       <Routes>
-
-        <Route path='/' element= {<Home/>}/>
-        <Route path='/collection' element= {<Collection/>}/>
-        <Route path='/about' element= {<About/>}/>
-        <Route path='/contact' element= {<Contact />}/>
-        <Route path='/product/:productId' element= {<Product />}/>
-        
-        <Route path='/cart' element= {<Cart/>}/>
-        <Route path='/login' element= {<Login/>}/>
-        <Route path='/place-order' element= {<PLaceOrder/>}/>
-        <Route path='/order' element= {<Orders/>}/>
-
-
+        <Route path="/" element={<Home />} />
+        <Route path="/collection" element={<Collection />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/place-order" element={<PLaceOrder />} />
+        <Route path="/order" element={<Orders />} />
       </Routes>
-      
-      
-    </div>
-  )
-}
 
-export default App
+     
+    </div>
+  );
+};
+
+export default App;
